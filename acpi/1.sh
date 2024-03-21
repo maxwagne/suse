b928013ea16c51b97819cb51e0950e5a6ff1d492
maxwagne <maxwagne@proton.me>
2024-03-21 06:43:15 +0100

    new

--- acpi/1.sh
+++ acpi/1.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-# Define the target directory
-target_dir="/usr/lib/pm-utils/sleep.d/"
+target_dir="/usr/lib/systemd/system-sleep/"
 
-# Check if the directory exists, if not, create it
 if [ ! -d "$target_dir" ]; then
     sudo mkdir -p "$target_dir"
 fi
 
-# Verify if the directory was created successfully
 if [ -d "$target_dir" ]; then
-    # Copy the file to the target directory with -p option
-    sudo cp -p fix "${target_dir}50_wakeupfix"
-
-    # Set ownership and permissions for the copied file
-    sudo chown root:root "${target_dir}50_wakeupfix"
-    sudo chmod 755 "${target_dir}50_wakeupfix"
-
+    sudo cp -p fix "${target_dir}50_wakeupfix.sh"
+    sudo chown root:root "${target_dir}50_wakeupfix.sh"
+    sudo chmod 755 "${target_dir}50_wakeupfix.sh"
     echo "Script deployed successfully."
 else
     echo "Failed to create directory: $target_dir"
